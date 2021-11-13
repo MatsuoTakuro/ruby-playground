48a69f499e6055a14b615d821e8d78fe6b313e5f
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2021-11-13 21:41:49 +0900

    5.3 Improve convert_length method

--- ruby-book/lib/convert_length.rb
+++ ruby-book/lib/convert_length.rb
@@ -1,3 +1,4 @@
-def convert_length(lengh, unit_from, unit_to)
-  39.37
+UNITS = { m: 1.0, ft: 3.28, in: 39.37 }.freeze # Ratios converted to meters
+def convert_length(length, from: :unit, to: :unit)
+  (length / UNITS[from] * UNITS[to]).round(2)
 end
--- ruby-book/playground.rb
+++ ruby-book/playground.rb
@@ -1,10 +1,1 @@
-fs = %w[apple melon orange]
-nums = [1, 2, 3]
-catch :done do
-  fs.each do |f|
-    nums.shuffle.each do |n|
-      puts "#{f}, #{n}"
-      throw :done if f == 'orange' && n == 3
-    end
-  end
-end
+puts Time.now.to_s
