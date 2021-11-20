f5fdd1928c49c283d1855ff4357c5c69d00a91b4
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2021-11-20 13:18:10 +0900

    Order of arguments in defining a method

--- ruby-book/playground.rb
+++ ruby-book/playground.rb
@@ -1,1 +1,4 @@
-puts Time.now.to_s
+def      foo(a,       b,       c = 3,   d = 4,    *ef,      g,       h,       i: 9,    j: 10,  **kl,          &block)
+       "a: #{a}, b: #{b}, c: #{c}, d: #{d}, ef: #{ef}, g: #{g}, h: #{h}, i: #{i}, j: #{j}, kl: #{kl}, block: #{block}"
+end
+puts foo(    1,       2,       3,       4,        5,6,      7,       8,       i: 9,    j: 10,  k:11,l:12)     { 13 }
