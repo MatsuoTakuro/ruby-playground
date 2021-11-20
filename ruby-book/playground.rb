63753d5c301dd5a09899a48a038e2c355bfd67c8
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2021-11-20 16:07:48 +0900

    Flexible manipulation of whitespaces

--- ruby-book/playground.rb
+++ ruby-book/playground.rb
@@ -1,13 +1,26 @@
-# https://qiita.com/jnchito/items/64c3fdc53766ac6f2008
-html = <<-HTML
-  <select name="game_console">
-  <option value="none"></option>
-  <option value="wii_u" selected>Wii U</option>
-  <option value="ps4">プレステ4</option>
-  <option value="gb">ゲームボーイ</option>
-  </select>
-HTML
+# https://qiita.com/jnchito/items/6f0c885c1c4929092578
+text = <<-TEXT
+def hello(name)
+  puts "Hello, \#{name}!"
+end
 
-replaced = html.gsub(%r{<option value="(\w+)"(?: selected)?>(.*?)</option>}, '\1,\2')
+hello('Alice')
+     
+hello('Bob')
+	
+hello('Carol')
+TEXT
+puts text.gsub(/^[ \t]+$/, '')
 
-puts replaced
+puts "\n------------------------------------------------\n\n"
+
+text2 = <<-TEXT
+Feb 14 07:33:02 app/web.1:  Completed 302 Found ...
+Feb 14 07:36:46 heroku/api:  Starting process ...
+Feb 14 07:36:50 heroku/scheduler.7625:  Starting ...
+Feb 14 07:36:50 heroku/scheduler.7625:  State ...
+Feb 14 07:36:54 heroku/router:  at=info method=...
+Feb 14 07:36:54 app/web.1:  Started HEAD "/" ...
+Feb 14 07:36:54 app/web.1:  Completed 200 ...
+TEXT
+puts text2.gsub(%r{^.+heroku/(api|scheduler).+\n}, '')
