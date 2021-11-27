357b30171c5118381322972ae75d217eb43760ae
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2021-11-28 06:30:12 +0900

    Learned about duck typing in ch.7.10.9

--- ruby-book/playground.rb
+++ ruby-book/playground.rb
@@ -1,40 +1,28 @@
 class Product
-  @name = 'Product'
-
-  def self.name
-    @name
-  end
-
-  def initialize(name)
+  def initialize(name, price)
     @name = name
+    @price = price
   end
 
-  def name
-    @name
+  def stock?
+    raise 'Must implement stock? in subclass'
+  end
+
+  def display_text
+    stock = stock? ? 'exists' : 'none'
+    puts "Item's name: #{@name}, price: #{@price}, stock: #{stock}"
   end
 end
 
 class DVD < Product
-  @name = 'DVD'
-
-  def self.name
-    @name
-  end
-
-  def upcase_name
-    @name.upcase
+  def stock?
+    true
   end
 end
 
-puts Product.name
-puts DVD.name
+product = Product.new('A great film', 1000)
 
-product = Product.new('An great movie')
-puts product.name
+# product.display_text
 
-dvd = DVD.new('An awesome film')
-puts dvd.name
-puts dvd.upcase_name
-
-puts Product.name
-puts DVD.name
+dvd = DVD.new('An awesome film', 3000)
+dvd.display_text
