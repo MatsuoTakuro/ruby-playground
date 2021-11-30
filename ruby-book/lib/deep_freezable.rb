e4b6a8dedb2cd99a13736220c384917e4c34ea77
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2021-11-30 20:22:11 +0900

    Make deep_freeze method acceptable to argument of Hash type

--- ruby-book/lib/deep_freezable.rb
+++ ruby-book/lib/deep_freezable.rb
@@ -1,6 +1,14 @@
 module DeepFreezable
   def deep_freeze(array_or_hash)
-    array_or_hash.each(&:freeze)
+    case array_or_hash
+    when Array
+      array_or_hash.each(&:freeze)
+    when Hash
+      array_or_hash.each do |key, value|
+        key.freeze
+        value.freeze
+      end
+    end
     array_or_hash.freeze
   end
 end
