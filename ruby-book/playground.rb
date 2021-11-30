7d349f1d1592a16338dd3c4bc986875cbaa8eca5
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2021-11-30 21:16:56 +0900

    Use module as namespace

--- ruby-book/playground.rb
+++ ruby-book/playground.rb
@@ -1,36 +1,19 @@
-module Loggable
-
-  def log(text)
-    puts "[LOG] #{text}"
+module Baseball
+  class Second
+    def initialize(player, uniform_number)
+      @player = player
+      @uniform_number = uniform_number
+    end
   end
 end
 
-class Product
-  extend Loggable
-
-  log 'Defined Product class.'
-
-  def self.create_products(names)
-    log 'crete_products is called.'
-  end
-
-  def title
-    puts 'A greate movie'
+module Clock
+  class Second
+    def initialize(digits)
+      @digits = digits
+    end
   end
 end
 
-class User
-  include Loggable
-  def name
-    log 'name is called.'
-    puts 'Alice'
-  end
-end
-
-product = Product.new
-product.title
-Product.create_products([])
-Product.log('Hello.')
-
-user = User.new
-user.name
+puts Baseball::Second.new('Alice', 13)
+puts Clock::Second.new(13)
