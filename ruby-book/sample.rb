cf867607dbf3a50bd4732ca1177af191650c1a32
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2021-12-02 10:59:52 +0900

    Abnormal termination in case of unexpected conditions

--- ruby-book/sample.rb
+++ ruby-book/sample.rb
@@ -1,16 +1,14 @@
-require 'date'
-
-def convert_heisei_to_date(heisei_text)
-  m = heisei_text.match(/平成(?<jp_year>\d+)年(?<month>\d+)月(?<day>\d+)日/)
-  year = m[:jp_year].to_i + 1998
-  month = m[:month].to_i
-  day = m[:day].to_i
-  begin
-    Date.new(year, month, day)
-  rescue ArgumentError
-    nil
+def currency_of(country)
+  case country
+  when :japan
+    'yen'
+  when :us
+    'dollar'
+  when :india
+    'rupee'
+  else
+    raise ArgumentError, "invalid name of country ; #{country}"
   end
 end
 
-puts convert_heisei_to_date('平成28年12月31日')
-puts convert_heisei_to_date('平成28年99月99日')
+currency_of(:italy)
