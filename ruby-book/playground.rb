9fbc127d8aad8cf769e2afb35ec4017ddc104255
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2021-12-03 20:52:21 +0900

    Learned about return value in error handling

--- ruby-book/playground.rb
+++ ruby-book/playground.rb
@@ -1,4 +1,13 @@
-File.open('some.txt', 'w') do |file|
-  file << 'Hello'
-  1 / 0
+def some_method(n)
+  begin
+    1 / n
+    'OK'
+  rescue
+    'error'
+  ensure
+    'ensure'
+  end
 end
+
+puts some_method(1)
+puts some_method(0)
