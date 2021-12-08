d0a20a8cfda3781330c082183f0b59ba92cbf1f1
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2021-12-08 10:03:11 +0900

    Learned about the detail of Proc object

--- ruby-book/playground.rb
+++ ruby-book/playground.rb
@@ -1,17 +1,62 @@
-def judge(age)
-  adult = Proc.new { |n| n > 20 }
-  child = Proc.new { |n| n < 20 }
+split_proc = :split.to_proc
+p split_proc
 
-  case age
-    when adult
-      'You are an adult.'
-    when child
-      'You are a child.'
-    else
-      'You are 20 years old.'
+p split_proc.call('a-b-c-d d')
+p 'a-b-c-d d'.split
+
+p split_proc.call('a-b-c-d d', '-', 3)
+p 'a-b-c-d d'.split('-', 3)
+
+def generate_proc(array)
+  counter = 0
+  Proc.new do
+    counter += 10
+    array << counter
   end
 end
 
-puts judge(25)
-puts judge(18)
-puts judge(20)
+values = []
+sample_proc = generate_proc(values)
+p values
+
+sample_proc.call
+p values
+
+sample_proc.call
+p values
+
+sample_proc.call
+p values
+
+sample_proc.call
+p values
+
+def proc_return
+  f = Proc.new { |n| return n * 10 }
+  ret = [1, 2, 3].map(&f)
+  "ret: #{ret}"
+end
+
+def lambda_return
+  f = ->(n) { return n * 10 }
+  ret = [1, 2, 3].map(&f)
+  "ret: #{ret}"
+end
+
+puts proc_return
+puts lambda_return
+
+# def proc_break
+#   f = Proc.new { |n| break n * 10 }
+#   ret = [1, 2, 3].map(&f)
+#   "ret: #{ret}"
+# end
+
+def lambda_break
+  f = ->(n) { break n * 10 }
+  ret = [1, 2, 3].map(&f)
+  "ret: #{ret}"
+end
+
+# puts proc_break
+puts lambda_break
