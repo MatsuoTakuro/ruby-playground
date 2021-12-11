dac6254fa470429bbb9a988b7bbab9b93ac44342
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2021-12-11 10:32:39 +0900

    Read/Write files in several formats

--- ruby-book/playground.rb
+++ ruby-book/playground.rb
@@ -23,3 +23,37 @@ lib = Pathname.new('./ruby-book/lib')
 puts lib.file?
 puts lib.directory?
 puts lib.join('some2.txt').to_s
+
+require 'csv'
+CSV.open('./ruby-book/lib/sample.csv', 'w') do |csv|
+  csv << %w[Name Email Age]
+  csv << ['Alice1', 'alice1@example.com', 21]
+  csv << ['Alice2', 'alice2@example.com', 22]
+end
+
+CSV.foreach('./ruby-book/lib/sample.csv', col_sep: ',') do |row|
+  puts "1: #{row[0]}, 2: #{row[1]}, 3: #{row[2]}"
+end
+
+CSV.foreach('./ruby-book/lib/sample.tsv', col_sep: ' ') do |row|
+  puts "1: #{row[0]}, 2: #{row[1]}, 3: #{row[2]}"
+end
+
+require 'json'
+user = { name: 'Alice', email: 'alice@example.com', age: 20 }
+user_json = user.to_json
+puts "json:\n#{user_json}"
+puts "json to hash:\n#{JSON.parse(user_json)}"
+
+require 'yaml'
+yaml = <<~TEXT
+alice:
+  name: 'Alice'
+  email: 'alice@example.com'
+  age: 20
+TEXT
+
+users = YAML.load(yaml)
+puts "yaml to hash:\n#{users}"
+users['alice']['gender'] = :female
+puts "yaml:\n#{YAML.dump(users)}"
