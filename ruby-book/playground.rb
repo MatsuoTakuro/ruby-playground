cc4ddd19bd9c296eaba655ce04352c239524f275
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2021-12-11 09:38:39 +0900

    Handle files and dirs

--- ruby-book/playground.rb
+++ ruby-book/playground.rb
@@ -1,25 +1,25 @@
-require 'logger'
+puts Time.new(2017, 1, 31, 23, 30, 59)
 
-from = 1
-to = 2
+require 'date'
+puts Date.new(2017, 1, 31)
+puts DateTime.new(2017, 1, 31, 23, 30, 59)
 
-distance = p to - from
-p distance
-puts distance
+puts File.exist?('./ruby-book/lib/fizz_buzz.rb')
+puts Dir.exist?('./ruby-book')
 
-a = 'hello'.tap { |s| puts "<<#{s}>>" }
-a
-
-'#043c78'.scan(/\w\w/).map(&:hex)
-hexs = '#043c78'.scan(/\w\w/).tap { |rgb| p rgb }.map(&:hex)
-puts hexs
-
-class User
-  def my_name_is_(name)
-    @name = name
-    Logger.new($stderr).debug "an argument \"name\" of my_name_is method: #{@name}"
-    puts "My name is #{@name}."
-  end
+File.open('./ruby-book/lib/fizz_buzz.rb', 'r') do |f|
+  puts f.readlines.count
 end
 
-puts User.new.my_name_is_('John')
+File.open('./some.txt', 'w') do |f|
+  f.puts 'Hello, world!'
+end
+
+require 'fileutils'
+# FileUtils.mv('./some_mv.txt', './some_mv2.txt')
+
+require 'pathname'
+lib = Pathname.new('./ruby-book/lib')
+puts lib.file?
+puts lib.directory?
+puts lib.join('some2.txt').to_s
