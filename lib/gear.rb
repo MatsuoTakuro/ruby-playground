57a6c174ff3ad78b13d471bd66650a3082be23d9
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2021-12-25 16:45:11 +0900

    Isolate wheel.diameter as an external message

--- lib/gear.rb
+++ lib/gear.rb
@@ -3,17 +3,23 @@
 class Gear
   attr_reader :chainring, :cog, :wheel
 
-  def initialize(chainring, cog, wheel = nil)
+  def initialize(chainring, cog, wheel)
     @chainring = chainring
     @cog = cog
     @wheel = wheel
+  end
+
+  def gear_inches
+    ratio * diameter
   end
 
   def ratio
     chainring / cog.to_f
   end
 
-  def gear_inches
-    ratio * wheel.diameter
+  private
+
+  def diameter
+    wheel.diameter
   end
 end
--- main.rb
+++ main.rb
@@ -3,9 +3,4 @@
 require './lib/gear'
 require './lib/wheel'
 
-@wheel = Wheel.new(26, 1.5)
-puts @wheel.circumference
-
-puts Gear.new(52, 11, @wheel).gear_inches
-
-puts Gear.new(52, 11).ratio
+puts Gear.new(52, 11, Wheel.new(26, 1.5)).gear_inches
