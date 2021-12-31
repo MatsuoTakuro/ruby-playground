6705e808b0fa6bcb2eadd14a1c05124eeecb6305
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2021-12-31 10:14:01 +0900

    Anti pattern on duck typing

--- main.rb
+++ main.rb
@@ -1,6 +1,46 @@
 # frozen_string_literal: true
 
-require './lib/gear'
-require './lib/wheel'
+class Trip
+  attr_reader :bicycles, :customers, :vehicle
 
-puts Wheel.new(52, 11, 26, 1.5).gear_inches
+  # The "mechanism" argument can be any class.
+  def prepare(preparers)
+    preparers.each do |preparer|
+      case preparer
+      when Mechanic
+        preparer.prepare_bicycles(bicycles)
+      when TripCoordinator
+        preparer.buy_food(customers)
+      when Driver
+        preparer.gas_up(vehicle)
+        preparer.fill_water_tank(vehicle)
+      end
+    end
+  end
+end
+
+class Mechanic
+  def prepare_bicycles(bicycles)
+    bicycles.each { |bicycle| prepare_bicycle(bicycle) }
+  end
+
+  def prepare_bicycle(bicycle)
+    puts bicycle
+  end
+end
+
+class TripCoordinator
+  def buy_food(customers)
+    puts customers
+  end
+end
+
+class Driver
+  def gas_up(vehicle)
+    puts vehicle
+  end
+
+  def fill_water_tank(vehicle)
+    puts vehicle
+  end
+end
