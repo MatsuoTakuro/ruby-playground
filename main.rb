d713f2a9d13b75dfee954be9f1551ff4494cf66f
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2022-01-01 14:08:19 +0900

    Anti pattern on classical inheritance

--- main.rb
+++ main.rb
@@ -1,65 +1,35 @@
-# frozen_string_literal: true
+class Bicycle
+  attr_reader :style, :size, :tape_color, :front_shock, :rear_shock
 
-class Trip
-  attr_reader :bicycles, :customers, :vehicle
-
-  def initialize(**arg)
-    @bicycles  = arg[:bicycles]
-    @customers = arg[:customers]
-    @vehicle   = arg[:vehicle]
+  def initialize(args)
+    @style = args[:style]
+    @size = args[:size]
+    @tape_color = args[:tape_color]
+    @front_shock = args[:front_shock]
+    @rear_shock = args[:rear_shock]
   end
 
-  def prepare(preparers)
-    preparers.each do |preparer|
-      # define public interface of "prepare_trip"
-      preparer.prepare_trip(self)
+  def spares
+    if style == :road
+      {
+        chain: '10-speed',
+        tire_size: '23', # milimeters
+        tape_color: tape_color
+      }
+    else
+      {
+        chain: '10-speed',
+        tire_size: '2.1', # inches
+        rear_shock: rear_shock
+      }
     end
   end
-
-  def buy_food(customers)
-    customers.each do |customer|
-      puts "#{customer} has an option to buy some food in this #{self.class.name.downcase}."
-    end
-  end
 end
 
-class Mechanic
-  # Implement public interface of "prepare_trip"
-  def prepare_trip(trip)
-    trip.bicycles.each { |bicycle| prepare_bicycle(bicycle) }
-  end
-
-  def prepare_bicycle(bicycle)
-    puts "#{bicycle} has got arranged by the #{self.class.name.downcase} in charge."
-  end
-end
-
-class TripCoordinator
-  # Implement public interface of "prepare_trip"
-  def prepare_trip(trip)
-    trip.buy_food(trip.customers)
-  end
-end
-
-class Driver
-  # Implement public interface of "prepare_trip"
-  def prepare_trip(trip)
-    vehicle = trip.vehicle
-    gas_up(vehicle)
-    fill_water_tank(vehicle)
-  end
-
-  def gas_up(vehicle)
-    puts "#{vehicle} has got gased up by the #{self.class.name.downcase} in charge."
-  end
-
-  def fill_water_tank(vehicle)
-    puts "The water tank of #{vehicle} has got filled with water by the #{self.class.name.downcase} in charge."
-  end
-end
-
-trip = Trip.new(bicycles: %w[A_bike B_bike C_bike],
-                customers: %w[A_san B_san C_san],
-                vehicle: 'A_car')
-
-trip.prepare([Mechanic.new, TripCoordinator.new, Driver.new])
+bike = Bicycle.new(
+  style: :moutain,
+  size: 's',
+  front_shock: 'Manitou',
+  rear_shock: 'Fox'
+)
+puts bike.spares
