569f170058bd44cf8ca1160b91b9ad487b827b5b
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2022-01-01 23:21:18 +0900

    Anti pattern 2 on classical inheritance

--- main.rb
+++ main.rb
@@ -26,10 +26,22 @@ class Bicycle
   end
 end
 
-bike = Bicycle.new(
-  style: :moutain,
-  size: 's',
-  front_shock: 'Manitou',
-  rear_shock: 'Fox'
-)
-puts bike.spares
+class MountainBike < Bicycle
+  attr_reader :front_shock, :rear_shock
+
+  def initialize(args)
+    @front_shock = args[:front_shock]
+    @rear_shock = args[:rear_shock]
+    super(args)
+  end
+
+  def spares
+    super.merge(rear_shock: rear_shock)
+  end
+end
+
+mountain_bike = MountainBike.new(size: 'S',
+                                 front_shock: 'Manitou',
+                                 rear_shock: 'Fox')
+mountain_bike.size
+mountain_bike.spares
