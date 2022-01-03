9cc568554558357a57a2cfa306c016c66b8beb73
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2022-01-03 23:57:55 +0900

    Verify(Test) command message

--- lib/gear.rb
+++ lib/gear.rb
@@ -1,10 +1,11 @@
 class Gear
-  attr_reader :chainring, :cog, :wheel
+  attr_reader :chainring, :cog, :wheel, :observer
 
   def initialize(args)
     @chainring = args[:chainring]
     @cog = args[:cog]
     @wheel = args[:wheel]
+    @observer = args[:observer]
   end
 
   def gear_inches
@@ -13,5 +14,19 @@ class Gear
 
   def ratio
     chainring / cog.to_f
+  end
+
+  def set_cog(new_cog)
+    @cog = new_cog
+    changed
+  end
+
+  def set_chainring(new_chainring)
+    @chainring = new_chainring
+    changed
+  end
+
+  def changed
+    observer.changed(chainring, cog)
   end
 end
